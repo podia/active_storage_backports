ce02cf0b206a66f168c75b567538b8b40707490b
Andrew Fomera <andrew@zerlex.net>
2020-08-24 14:03:53 -0600

    initial commit with most of code backported
    One change is the minimum token length for blobs is not the same as 6.1 master

--- lib/active_storage_backports.rb
+++ lib/active_storage_backports.rb
@@ -1,6 +1,16 @@
 require "active_storage_backports/version"
+require "active_record/signed_id"
+require "active_support/configuration_file"
+require "active_support/core_ext/module/delegation"
+require "active_storage"
 
 module ActiveStorageBackports
   class Error < StandardError; end
   # Your code goes here...
+end
+
+module ActiveRecord
+  class Base
+    include SignedId
+  end
 end
